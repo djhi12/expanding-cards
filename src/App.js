8c8ebda703dd1d48ffe9af17f5bfedda56968601
djhi12 <ido20002@byui.edu>
2023-08-28 20:15:30 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'; // Import React and useEffect
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
 function App() {
+  useEffect(() => { // Use useEffect to handle DOM manipulation
+    const panels = document.querySelectorAll('.panel');
+
+    function handlePanelClick(panel) {
+      removeActiveClasses();
+      panel.classList.add('active');
+      console.log(panel);
+    }
+
+    function removeActiveClasses() {
+      panels.forEach(panel => {
+        panel.classList.remove('active');
+      });
+    }
+
+    panels.forEach(panel => {
+      panel.addEventListener('click', () => handlePanelClick(panel));
+    });
+
+    return () => { // Cleanup event listeners
+      panels.forEach(panel => {
+        panel.removeEventListener('click', () => handlePanelClick(panel));
+      });
+    };
+  }, []); // Empty dependency array to run the effect only once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expanding Cards</h1>
+
+      <div className='img-container d-flex justify-content-center'>
+        <div className='img-container__img-box panel'>
+          <p className='panel__text'>Image 1</p>
+        </div>
+        <div className='img-container__img-box panel'>
+          <p className='panel__text'>Image 2</p>
+        </div>
+        <div className='img-container__img-box panel'>
+          <p className='panel__text'>Image 3</p>
+        </div >
+        <div className='img-container__img-box panel'>
+          <p className='panel__text'>Image 4</p>
+        </div>
+        <div className='img-container__img-box panel'>
+          <p className='panel__text'>Image 5</p>
+        </div>
+      </div>
     </div>
   );
 }
